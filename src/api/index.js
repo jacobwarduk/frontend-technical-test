7fef573c026667673191474ccbcfb0d00554eb46
Jacob Ward <jacob@candyboxmedia.com>
2017-05-24 10:51:59 +0100

    get set up to use vue.js

--- src/api/index.js
+++ src/api/index.js
@@ -12,7 +12,3 @@ export const getData = url => {
 		req.send();
 	});
 };
-
-export const addItem = item => {
-
-};
--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import { getData } from './api/index.js';
 import { apiVehicleUrl } from './constants.js';
 
+
 getData(apiVehicleUrl).then(res => {
 	const vehicles = JSON.parse(res).vehicles;
 
+
+	console.log(vehicles);
+
+
 	return vehicles.reduce((promise, vehicle) => {
 		return promise.then(() => {
+
+
 			console.log(vehicle);
+
+
 		}).then(() => {
 			getData(`${apiVehicleUrl}/${vehicle.id}`).then(res => {
 				// Add to page
