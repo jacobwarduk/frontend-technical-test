b4c08a7f9e080cbe1c9a1641b9a63de7866e1771
Jacob Ward <jacob@candyboxmedia.com>
2017-05-24 10:50:48 +0100

    fix build sequence and add vue.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,17 @@ const source = require('vinyl-source-stream');
 const sass = require('gulp-sass');
 const mocha = require('gulp-mocha');
 const server = require('gulp-develop-server');
+const fs = require("fs");
+const del = require("del");
+const runSequence = require("run-sequence");
+
 
 gulp.task('js', function () {
    return browserify({entries: './src/app.js', extensions: ['.js'], debug: true})
-        .transform('babelify', {presets: ['es2015', 'react']})
+        .transform('babelify', {presets: ['es2015']})
         .bundle()
         .pipe(source('app.js'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('js:watch', function () {
@@ -22,7 +26,7 @@ gulp.task('js:watch', function () {
 gulp.task('sass', function () {
     return gulp.src('./src/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/styles'));
 });
 
 gulp.task('sass:watch', function () {
@@ -45,13 +49,48 @@ gulp.task('server:watch', function () {
     gulp.watch( [ './server.js' ], server.restart );
 });
 
-gulp.task('vendors:install', function() {
-    return gulp.src([
-        './node_modules/foundation-grid/grid.css'
-      ])
-      .pipe(gulp.dest('./dist'));
+gulp.task('vendor:install:scripts', function() {
+	return browserify({entries: './node_modules/vue/dist/vue.esm.js', extensions: ['.js'], debug: true})
+		.transform('babelify', {presets: ['es2015']})
+		.bundle()
+		.pipe(source('vue.esm.js'))
+		.pipe(gulp.dest('./dist/scripts'));
 });
 
-gulp.task('default', function () {
-  gulp.start('vendors:install', 'sass', 'sass:watch', 'js', 'js:watch', 'server', 'server:watch');
+gulp.task('vendor:install:styles', function () {
+	return gulp.src([
+        './node_modules/foundation-grid/grid.css'
+      ])
+		.pipe(gulp.dest('./dist/styles'));
 });
+
+gulp.task('clean:dist', function (done) {
+	if (fs.existsSync('./dist')) {
+		return del('./dist/*');
+	}
+	done();
+});
+
+gulp.task('default', function (done) {
+	runSequence(
+		'clean:dist',
+		[
+			'vendor:install:scripts',
+			'vendor:install:styles',
+			'sass',
+			'js'
+		],
+		[
+			'sass:watch',
+			'js:watch'
+		],
+		'server',
+		'server:watch',
+		function callback() {
+			return done;
+		}
+	)
+});
+
+
+	//gulp.start('clean:dist', 'vendor:install:scripts', 'vendor:install:styles', 'sass', 'sass:watch', 'js', 'js:watch', 'server', 'server:watch');});
